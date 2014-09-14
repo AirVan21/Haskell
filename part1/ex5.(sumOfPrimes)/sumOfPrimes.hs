5e8e804b333b4a1b83f8915615c31ffa245f7e6f
AirVan21 <airvan21@gmail.com>
2014-09-14 11:49:43 +0400

    Added muddy logic case

--- part1/ex5.(sumOfPrimes)/sumOfPrimes.hs
+++ part1/ex5.(sumOfPrimes)/sumOfPrimes.hs
@@ -1,7 +1,18 @@
--- Main function --
+-- Prime test function --
+isPrime 1 = False
 isPrime number = isPrimeHelp number (div number 2) True
--- Help function --
+-- Prime test help function --
 isPrimeHelp number 1 answer = answer
 isPrimeHelp number divider answer = if (mod number divider) == 0
 									then isPrimeHelp number 1 False
 									else isPrimeHelp number (divider - 1) True
+-- Main function -- 
+g number = gHelp 1 (number - 1) False 
+-- Base result --
+gHelp 1 1 result = result
+-- Help function --
+gHelp numOne numTwo result = if ((isPrime numOne) && (isPrime numTwo) && (numOne /= numTwo))
+							 then gHelp 1 1 True 
+							 else if (numTwo > numOne)
+							 	  then gHelp (numOne + 1) (numTwo - 1) False 
+							 	  else gHelp 1 1 False	
