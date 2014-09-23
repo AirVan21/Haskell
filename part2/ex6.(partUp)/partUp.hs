06fd20a0fcbd19b116c5ac6aa1117aa3574235de
AirVan21 <airvan21@gmail.com>
2014-09-23 18:31:32 +0400

    Goes OK with first tests, but wrong for long first seq

--- part2/ex6.(partUp)/partUp.hs
+++ part2/ex6.(partUp)/partUp.hs
@@ -1,21 +1,26 @@
 -- Main function --
 parts list = up list 1 True
+
+{--
+	My idea:
+	
+	1) Find first upSequence and count it's Length (up)
+	2) Check other upSequnces for this Length condition (upWithLength)
+
+--}
+
+
 -- Function for defining first up sequence length --
+up [_] seqLen result = result
 up (top:pretop:rest) seqLen result = if (top < pretop)
 					  		  		 then up (pretop:rest) (seqLen + 1) True
 					   		  		 else if (top == pretop) 
 					   			   		  then up [] seqLen False
-					   			   		  else upWithLenght (pretop:rest) 1 seqLen result
-up [_] seqLen result = result
--- Checking all up sequences -- 
-upWithLenght [] _ _ result = result
-upWithLenght (top:pretop:rest) curLen seqLen result = if (top < pretop)
-					  		  		 				  then if (curLen < seqLen) 
-					  		  		 				  	   then upWithLenght (pretop:rest) (curLen + 1) seqLen True
-					  		  		 				  	   else upWithLenght [] curLen seqLen False
-					   		  		 				  else if (top > pretop)
-					   		  		 				  	   then if (curLen == seqLen)
-					   		  		 				  	   	    then upWithLenght (pretop:rest) 1 seqLen True
-					   		  		 				  	   	    else upWithLenght [] curLen seqLen False
-					   		  		 				  	   else upWithLenght [] curLen seqLen False
-upWithLenght element _ _ result = result
+					   			   		  else upWithLenght (pretop:rest) 1 seqLen 
+-- Checking all up sequences --  
+upWithLenght [_] curLen seqLen = (curLen == seqLen)
+upWithLenght (top:pretop:rest) curLen seqLen = if (seqLen == curLen)
+											   then upWithLenght (pretop:rest) 1 seqLen 
+											   else if (top < pretop)
+											   	    then upWithLenght (pretop:rest) (curLen + 1) seqLen 
+											   	    else upWithLenght [] 0 seqLen
