3032cb79eee908b6ade54b0d71b06e0ab30f7439
AirVan21 <airvan21@gmail.com>
2014-09-23 23:21:31 +0400

    Min up seq counter

--- part2/ex6.(partUp)/partUp.hs
+++ part2/ex6.(partUp)/partUp.hs
@@ -1,22 +1,23 @@
 -- Main function --
-parts list = up list 1 True
+parts list = upWithLenght list 1 (up list 1 [])  
 
 {--
 	My idea:
 	
-	1) Find first upSequence and count it's Length (up)
-	2) Check other upSequnces for this Length condition (upWithLength)
+	1) Find upSequences and count it's Lengths (up)
+	2) Check all upSequnces for minimum Length condition (upWithLength)
 
 --}
 
 
--- Function for defining first up sequence length --
-up [_] seqLen result = result
-up (top:pretop:rest) seqLen result = if (top < pretop)
-					  		  		 then up (pretop:rest) (seqLen + 1) True
-					   		  		 else if (top == pretop) 
-					   			   		  then up [] seqLen False
-					   			   		  else upWithLenght (pretop:rest) 1 seqLen 
+-- Function for defining up sequences lengths --
+up [] _ [] = 0
+up [_] seqLen lenList = minimum (seqLen:lenList) 
+up (top:pretop:rest) seqLen lenList = if (top < pretop)
+					  		  		  then up (pretop:rest) (seqLen + 1) lenList
+					   		  		  else if (top == pretop) 
+					   			   		   then up [] seqLen [] 
+					   			   		   else up (pretop:rest) 1 (seqLen:lenList) 
 -- Checking all up sequences --  
 upWithLenght [_] curLen seqLen = (curLen == seqLen)
 upWithLenght (top:pretop:rest) curLen seqLen = if (seqLen == curLen)
