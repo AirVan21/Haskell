b4afe4b685a594072b03c9c9aa0f05212eebc7ec
AirVan21 <airvan21@gmail.com>
2014-09-28 00:43:18 +0400

    Last edition with test fix

--- part2/ex6.(partUp)/partUp.hs
+++ part2/ex6.(partUp)/partUp.hs
@@ -1,9 +1,15 @@
 -- Main function --
-parts list = let					
+parts list =let 
 				lengths = up list 1 []
-			 	minLength = minimum lengths
-				delimeters = searchDelimeters minLength [] minLength
- 			in testAllDelimeters delimeters lengths True 
+			in if (lengths /= [])
+			   then parts' list lengths
+			   else False
+
+-- Fix for parts [1, 2, 2, 0, 1, 1] --
+parts' list lengths = let					
+			 			minLength = minimum lengths
+						delimeters = searchDelimeters minLength [] minLength
+ 			 		  in testAllDelimeters delimeters lengths True 
  			
 {--
 	My idea:
@@ -12,7 +18,7 @@ parts list = let
 	2) Find minimum length
 	3) Find all delimeters for minimum length
 	4) Check if these delimeters are delimeters for all lengths
-		
+
 --}
 
 
@@ -23,21 +29,12 @@ up (top:pretop:rest) seqLen lenList = if (top < pretop)
 					  		  		  then up (pretop:rest) (seqLen + 1) lenList
 					   		  		  else if (top == pretop) 
 					   			   		   then up [] seqLen [] 
-					   			   		   else up (pretop:rest) 1 (seqLen:lenList) 
-
--- Checking all up sequences --  
-upWithLenght [_] curLen seqLen = (curLen == seqLen)
-upWithLenght (top:pretop:rest) curLen seqLen = if (seqLen == curLen)
-											   then upWithLenght (pretop:rest) 1 seqLen 
-											   else if (top < pretop)
-											   	    then upWithLenght (pretop:rest) (curLen + 1) seqLen 
-											   	    else upWithLenght [] 0 seqLen
+					   			   		   else up (pretop:rest) 1 (seqLen:lenList)
 -- Looking for delimeters -- 
 searchDelimeters _ delimList 1 = delimList
 searchDelimeters dividend delimList counter = if ((mod dividend counter) == 0)
 										      then searchDelimeters dividend (counter:delimList) (counter - 1)     
 										      else searchDelimeters dividend delimList (counter - 1)
-
 -- Cheking that current delimeter fits for lengths -- 
 checkDelimeter _ [] result = result 
 checkDelimeter delimeter (top:rest) result = if ((mod top delimeter) == 0)
