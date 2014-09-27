80ca2bf98d25720e0ffb5a1103bbc074b6aeec11
AirVan21 <airvan21@gmail.com>
2014-09-27 22:24:16 +0400

    Filled info for test value

--- part3/ex5.(candyShop)/candyShop.hs
+++ part3/ex5.(candyShop)/candyShop.hs
@@ -1,3 +1,9 @@
--- Main fucntion -- 
-data smth1 = Good1 Integer Double
-data smth2 = Good2 Integer Double Double
+-- Type declaration -- 
+data Candy = Cake String Double | Sweets String Double Double
+-- Price calculation  -- 
+price (Cake _ cost) = cost
+price (Sweets _ cost weight) = cost * weight -- weight in kg
+totalPrice candyList = sum (map price candyList)
+-- Test (totalPrice == 1000) --
+test = totalPrice [(Cake "Наполеон" 350), (Sweets "Маска" 400 0.5), (Sweets "Raffaello" 600 0.250), (Cake "Тирамису" 300)]
+ 
