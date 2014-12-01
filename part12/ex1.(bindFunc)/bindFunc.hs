c3cbbfd3ffa045bf96b44af0ca9785020671bede
AirVan21 <airvan21@gmail.com>
2014-12-02 01:13:07 +0300

    Cool >>>= operator

---
+++ part12/ex1.(bindFunc)/bindFunc.hs
@@ -0,0 +1,19 @@
+{-- Task 
+
+	Using '>>>=' from slides Implement  
+
+--}
+
+-- Default find definition --
+find cond [] = (0, [])
+find cond (x:xs) = if cond x then (x, xs) else find cond xs
+
+-- Cool operator implementation --
+func1 >>>= func2 = \xs -> let
+                             (x, rest) = func1 xs
+                          in func2 x rest
+
+-- Really? --
+return1 value xs = (value, xs) 
+
+f = find (>3) >>>= \x -> find (>x) >>>= \y -> return1 (x + y) 
